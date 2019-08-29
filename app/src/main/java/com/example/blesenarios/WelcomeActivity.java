e76567d3707fc45444e8f2c560235272dca5b783
sedali <seyedalian@outlook.com>
2019-08-29 16:42:27 +0430

    progress line add to welcome activity

--- app/src/main/java/com/example/blesenarios/WelcomeActivity.java
+++ app/src/main/java/com/example/blesenarios/WelcomeActivity.java
@@ -1,25 +1,35 @@
 package com.example.blesenarios;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class WelcomeActivity extends AppCompatActivity {
-
+    TextView progress_tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        progress_tv = findViewById(R.id.progress_tv);
+        writeDot(15,100);
+    }
 
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(WelcomeActivity.this , MainActivity.class));
-                finish();
-            }
-        },2000);
+    private void writeDot(final int number, final int delay) {
+        if(number<=0){
+            startActivity(new Intent(WelcomeActivity.this , MainActivity.class));
+            finish();
+        }
+        else {
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    writeDot(number-1,delay);
+                }
+            },delay);
+            progress_tv.append("_");
+        }
     }
 }
